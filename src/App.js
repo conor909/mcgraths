a5d7ef43dd8ad24de1ca42b09ba99e85d9e2bf85
Conor McGrath <conor@homelyfe.com>
2018-02-27 20:51:36 +0000

    template

--- src/App.js
+++ src/App.js
@@ -6,13 +6,38 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+
+        <header>
+          <nav className="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+            <a className="navbar-brand" href="#">J&S McGrath</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                  <a className="nav-link" href="#">About</a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <main role="main" className="container" style={{ padding: '60px' }}>
+          <h1 className="mt-5">Sticky footer with fixed navbar</h1>
+          <p className="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
+          <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
+        </main>
+
+        <footer className="footer">
+          <div className="container">
+            <span className="text-muted">J&S McGrath</span>
+          </div>
+        </footer>
+
       </div>
     );
   }
