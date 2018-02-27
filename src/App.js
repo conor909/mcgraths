3533c66556967a0fd2cf12ef03aa878fd856bf4a
Conor McGrath <conor@homelyfe.com>
2018-02-27 21:02:25 +0000

    fluid css

--- src/App.js
+++ src/App.js
@@ -26,14 +26,13 @@ class App extends Component {
           </nav>
         </header>
 
-        <main role="main" className="container" style={{ padding: '60px' }}>
-          <h1 className="mt-5">Sticky footer with fixed navbar</h1>
-          <p className="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body &gt; .container</code>.</p>
-          <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
+        <main role="main" className="container-fluid" style={{ paddingTop: '57px' }}>
+          <h1 className="mt-5">J&S McGraths Ltd.</h1>
+          <p className="lead">Loreum Ipsum</p>
         </main>
 
         <footer className="footer">
-          <div className="container">
+          <div className="container-fluid">
             <span className="text-muted">J&S McGrath</span>
           </div>
         </footer>
